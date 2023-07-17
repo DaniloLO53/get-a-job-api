746b5f08c9a2106ccbae3f63a07f51c6f665a1b0
Danilo Leão <olin_ad@hotmail.com>
2023-07-17 10:49:41 -0300

    feat: total changed schema to new app purpose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import configuration from './config/configuration';
 import { JobModule } from './jobs/job.module';
 import { UserModule } from './user/user.module';
 
